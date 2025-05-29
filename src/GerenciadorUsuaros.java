f15428fa21fb1f2875913c3b37689b47476d4884
GUILHERME RIBEIRO GOMES <guilherme.2988@aluno.pr.senac.br>
2025-05-28 21:44:36 -0300

    first commit

---
+++ src/GerenciadorUsuaros.java
@@ -0,0 +1,10 @@
+public class GerenciadorUsuaros {
+    import java.util.ArrayList;
+    public class GerenciadorUsuarios {
+        public ArrayList<Usuario> usuarios = new ArrayList<>();
+        public void adicionarUsuario(Usuario u) {
+            usuarios.add(u);
+        }
+    }
+
+}
